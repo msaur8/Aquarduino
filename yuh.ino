a13c21f66ef9084511aaa96eed5068919c820ffa
akhilaajohny <akhila@ucsb.edu>
2025-02-19 10:48:06 -0800

    pump code

--- yuh.ino
+++ yuh.ino
@@ -1,9 +1,20 @@
+int pump = ; //pin for pump
+int mositure = ; //pin for misture sensor
+
 void setup() {
-  // put your setup code here, to run once:
-yuhhh
+  Serial.begin(9600);
+pinMode(pump, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  int moistureRaw = analogRead() //(pin for humidity sensor) - reads sensor value
+  int moistureReal = map(humidityRaw, 1023, 0, 0, 100); // - map(value, fromLOW, fromHIGH, toLOW, toHIGH)
+  Serial.println(humidityReal);
+  delay(100);
+  if (moistureReal < 30) //if moisture level is below 30% pump turns on, otherwise it turns off
+  {
+    digitalWrite(pump, HIGH);
+  }else {
+    digitalwrite(pump, LOW);
+  }
 }
